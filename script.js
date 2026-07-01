d5c2c53bcbe5de574baada731c9f9a73f20ced83
Abhay Chaturvedi <109450662+abhay707@users.noreply.github.com>
2026-07-01 17:43:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ function makeid(l) {
 	let char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
 
 	for(let i = 0;i<l;i++){
-		ans += char_list.charAt(Math.floor(Math.random()*char_list.length)
+		ans += char_list.charAt(
+			Math.floor(
+				Math.random()*char_list.length
+			)
+		)
 	}
 
 	return ans;
