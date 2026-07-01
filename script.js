d4d673957fd739f575abfbcec23421e535bd7616
Abhay Chaturvedi <109450662+abhay707@users.noreply.github.com>
2026-07-01 17:41:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 function makeid(l) {
-  // write your code here
+	let ans = "";
+	let char_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+
+	for(let i = 0;i<l;i++){
+		ans += char_list.charAt(Math.floor(Math.random()*char_list.length)
+	}
+
+	return ans;
 }
 
-// Do not change the code below.
 const l = prompt("Enter a number.");
 alert(makeid(l));
